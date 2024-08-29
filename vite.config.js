5c8577752df6a215a9aff7b20530d1d2764c3099
Jarosław Święs <jaroslaw.main@gmail.com>
2024-08-29 15:01:07 +0200

    gh-pages vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
-base: "/Project-Manager-App"
+
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Project-Manager-App",
   plugins: [react()],
 })
